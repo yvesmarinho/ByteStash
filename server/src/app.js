774277b4ad357422409c56eb6ecb2cfb620144cf
Jordan Dalby <jordan.dalby1@outlook.com>
2024-10-23 22:44:53 +0100

    Refactor app.js to add CORS headers and security measures

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const { initializeDatabase } = require('./config/database');
 const snippetRoutes = require('./routes/snippetRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.set('trust proxy', true);
+app.use((req, res, next) => {
+  res.header('X-Powered-By', null);
+  res.header('X-Frame-Options', 'SAMEORIGIN');
+  res.header('X-Content-Type-Options', 'nosniff');
+  next();
+});
 
 app.use('/api/snippets', snippetRoutes);
 
