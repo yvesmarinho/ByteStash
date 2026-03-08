9e1c7cf03b634e41208efcd2077cd7ce3ef145c7
紅坂柚月 <akasaka@ydk.vc>
2026-03-08 23:17:17 +0900

    [fix] Mistake of not including ja locale

--- client/src/i18n/constants.ts
+++ client/src/i18n/constants.ts
@@ -4,6 +4,7 @@ export const LOCALE_ISO_CODES: Record<Locale, string> = {
   [Locale.en]: 'en-US',
   [Locale.ru]: 'ru-RU',
   [Locale.es]: 'es-ES',
+  [Locale.ja]: 'ja-JP',
 };
 export const DEFAULT_LOCALE: Locale = Locale.en;
 export const DEFAULT_LOCALE_ISO_CODE = LOCALE_ISO_CODES[Locale.en];
