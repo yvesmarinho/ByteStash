8a0951d1aa709fd5a5171baaf71ce3d0475aedc7
cyberalby2 <enigmalspd1@gmail.com>
2026-03-26 17:50:36 +0100

    add it resource in index.ts file

--- client/src/i18n/resources/index.ts
+++ client/src/i18n/resources/index.ts
@@ -3,6 +3,7 @@ import { resources as ruResources } from './ru';
 import { resources as esResources } from './es';
 import { resources as jaResources } from './ja';
 import { resources as zhResources } from './zh';
+import { resources as itResources } from './it';
 
 export const resources = {
   en: enResources,
@@ -10,4 +11,5 @@ export const resources = {
   es: esResources,
   ja: jaResources,
   zh: zhResources,
+  it: itResources
 };
